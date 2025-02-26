a2ef417792d3676b5cdf0bf58ae49a510cf32e2d
aznos <pigiotyreal@gmail.com>
2025-02-26 17:04:51 -0500

    VarInt and String custom data types

---
+++ src/main/kotlin/datatypes/StringType.kt
@@ -0,0 +1,48 @@
+package com.aznos.datatypes
+
+import com.aznos.datatypes.VarInt.readVarInt
+import com.aznos.datatypes.VarInt.writeVarInt
+import java.io.DataInputStream
+import java.io.IOException
+import java.io.OutputStream
+import java.nio.charset.StandardCharsets
+
+/**
+ * Utility class for handling strings with VarInt prefixed length encoding
+ *
+ * This format is used in communication where a string is stored as
+ * a VarInt length followed by the UTF-8 encoded byte sequence
+ */
+object StringType {
+    /**
+     * Reads a string from the [DataInputStream]
+     *
+     * The method first reads a VarInt that represents the string length then read the
+     * corresponding number of bytes and decodes them as a UTF-8 string
+     *
+     * @return Decoded string
+     * @throws IOException If an I/O error occurs while reading the input stream
+     */
+    @Throws(IOException::class)
+    fun DataInputStream.readString(): String {
+        val data = ByteArray(readVarInt())
+        readFully(data)
+
+        return String(data, StandardCharsets.UTF_8)
+    }
+
+    /**
+     * Writes a string to the [OutputStream]
+     *
+     * The method writes a string length as a VarInt, then write the UTF-8 encoded bytes
+     *
+     * @param value The string to encode
+     * @throws IOException If an I/O error occurs while reading the input stream
+     */
+    @Throws(IOException::class)
+    fun OutputStream.writeString(value: String) {
+        val data = value.toByteArray(StandardCharsets.UTF_8)
+        writeVarInt(data.size)
+        write(data)
+    }
+}
---
+++ src/main/kotlin/datatypes/VarInt.kt
@@ -0,0 +1,64 @@
+package com.aznos.datatypes
+
+import java.io.DataInputStream
+import java.io.IOException
+import java.io.OutputStream
+import kotlin.experimental.and
+import kotlin.jvm.Throws
+
+/**
+ * Utility class that provides methods for encoding and decoding VarInts
+ *
+ * VarInts are variable length integers that use byte wise encoded system where
+ * the most signification bit of each byte indicates whether more bytes follow
+ *
+ * @see <a href="https://minecraft.wiki/w/Minecraft_Wiki:Projects/wiki.vg_merge/VarInt_And_VarLong">Wiki on VarInts</a>
+ */
+object VarInt {
+    /**
+     * Reads a VarInt from the [DataInputStream]
+     *
+     * @return The decoded integer
+     * @throws IOException If an I/O error occurs while reading the input stream
+     * @throws RuntimeException If the VarInt exceeds 5 bytes (out of range)
+     */
+    @Throws(IOException::class)
+    fun DataInputStream.readVarInt(): Int {
+        var numRead = 0
+        var result = 0
+        var read: Byte
+
+        do {
+            read = readByte()
+
+            val value = read.toInt() and 0b01111111
+            result = result or (value shl (7 * numRead))
+
+            numRead++
+            if(numRead > 5) throw RuntimeException("VarInt too big")
+        } while((read.toInt() and 0b10000000) != 0)
+
+        return result
+    }
+
+    /**
+     * Writes a VarInt to the [OutputStream]
+     *
+     * @param value The integer to be encoded
+     * @throws IOException If an I/O error occurs while reading the input stream
+     */
+    @Throws(IOException::class)
+    fun OutputStream.writeVarInt(value: Int) {
+        var tempVal = value
+        do {
+            var temp = (tempVal and 0b01111111).toByte()
+            tempVal = tempVal ushr 7
+
+            if(tempVal != 0) {
+                temp = (temp.toInt() or 0b10000000).toByte()
+            }
+
+            write(temp.toInt())
+        } while(tempVal != 0)
+    }
+}
