b6e3c5792329ba5ff25894bd55e49688b887686f
aznos <pigiotyreal@gmail.com>
2025-03-02 18:04:12 -0500

    Added detekt to project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm") version "2.1.0"
     kotlin("plugin.serialization") version "2.1.10"
+    id("io.gitlab.arturbosch.detekt") version "1.23.8"
     application
 }
 
@@ -15,6 +16,11 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.0")
     implementation("com.google.code.gson:gson:2.12.1")
     implementation("dev.dewy:nbt:1.5.1")
+}
+
+detekt {
+    buildUponDefaultConfig = true
+    config.setFrom("$projectDir/config/detekt/detekt.yml")
 }
 
 application {
