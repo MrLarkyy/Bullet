2eade5094413fc10000e193b02b873ebeb91742c
MrLarkyy_ <studiotadikl@gmail.com>
2025-03-09 02:14:32 +0100

    Add ServerUpdateLightPacket to handle light update logic
    This commit introduces a new packet class, `ServerUpdateLightPacket`, used for updating light data in server communication. It includes chunk coordinates and light data serialization logic for efficient data transfer.

---
+++ src/main/kotlin/packets/play/out/ServerUpdateLightPacket.kt
@@ -0,0 +1,28 @@
+package com.aznos.packets.play.out
+
+import com.aznos.datatypes.VarInt.writeVarInt
+import com.aznos.packets.data.chunk.LightData
+import com.aznos.packets.newPacket.Keyed
+import com.aznos.packets.newPacket.ResourceLocation
+import com.aznos.packets.newPacket.ServerPacket
+
+class ServerUpdateLightPacket(
+    var x: Int,
+    var z: Int,
+    var lightData: LightData
+): ServerPacket(key) {
+
+    companion object {
+        val key = Keyed(0x2B, ResourceLocation.vanilla("play.out.light_update"))
+
+    }
+
+    override fun retrieveData(): ByteArray {
+        return writeData {
+            writeVarInt(x)
+            writeVarInt(z)
+            LightData.write(this, lightData)
+        }
+    }
+
+}
